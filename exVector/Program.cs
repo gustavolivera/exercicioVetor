a4a9112a84877ba9b0959c6b0f6394972e4389d6
gustavolivera <social.gustavosilva@gmail.com>
2024-09-13 15:08:39 -0300

    Add project files.

---
+++ exVector/Program.cs
@@ -0,0 +1,32 @@
+﻿using exVector;
+
+Console.Write("How many rooms will be rented? ");
+Console.WriteLine();
+
+int n = int.Parse(Console.ReadLine());
+
+Rent[] vector  = new Rent[10];
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Rent #" + (i+1) + ": ");
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Email: ");
+    string email = Console.ReadLine();
+    Console.Write("Room: ");
+    int room = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    vector[room] = new Rent{Name = name, Email = email, Room = room};
+}
+
+Console.WriteLine("Busy rooms: ");
+
+for(int i = 0; i < 10; i++)
+{
+    if (vector[i] != null)
+    {
+        Console.WriteLine(vector[i]);
+    }
+}
---
+++ exVector/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exVector
+{
+	internal class Rent
+	{
+		public string Name { get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+
+		public override string ToString()
+		{
+			return Room + ": " + Name + ", " + Email;
+		}
+
+	}
+
+
+}
